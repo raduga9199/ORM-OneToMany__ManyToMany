8786847a9929b3d848ec69ad5507f30f00649fe3
raduga9199 <raduga9199@gmail.com>
2023-07-12 08:44:41 -0400

    Items and Carts - @ManyToMany

---
+++ src/main/java/com/cydeo/model/Cart.java
@@ -0,0 +1,26 @@
+package com.cydeo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Carts")
+@Data
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    private List<Item> itemList;
+
+
+
+
+
+}
